c09534ecde62bc54954c8640c957d74763156cac
Jubraj001 <jubrajdev001@gmail.com>
2022-11-25 20:26:39 +0530

    Completed markers

--- Jubraj/js/map.js
+++ Jubraj/js/map.js
@@ -23,27 +23,53 @@ var behavior = new H.mapevents.Behavior(mapEvents);
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(position => {
-        console.log(position);
         let browserPosition = {lat:position.coords.latitude,lng:position.coords.longitude};
-        // var posIcon = new H.map.Icon('img/marker_icon.png');
-        let marker = new H.map.Marker(browserPosition);
+        var hospital_url = new URL(`https://places.ls.hereapi.com/places/v1/autosuggest?at=${browserPosition.lat},${browserPosition.lng}&q=hospital&apiKey=${window.hereCreds.JS_KEY}`);
+        fetch(hospital_url).then(response => response.json()).then(data => {
+            for(var i=0;i<data.results.length;i++)
+            {
+                if(data.results[i].position!=undefined)
+                {
+                    var latitude = data.results[i].position[0];
+                    var longitude = data.results[i].position[1];
+                    let hosp_position = {lat:latitude,lng:longitude};
+                    var hospicon= new H.map.Icon('img/hospmark.png',{size: {w:32,h:32}});
+                    var hosp_marker = new H.map.Marker(hosp_position,{icon: hospicon});
+                    map.addObject(hosp_marker);
+                }
+            }}).catch(err => console.log(err));
+        var police_url = new URL(`https://places.ls.hereapi.com/places/v1/autosuggest?at=${browserPosition.lat},${browserPosition.lng}&q=police&apiKey=${window.hereCreds.JS_KEY}`);
+        fetch(police_url).then(response => response.json()).then(data => {
+            for(var i=0;i<data.results.length;i++)
+            {
+                if(data.results[i].position!=undefined)
+                {
+                    var latitude = data.results[i].position[0];
+                    var longitude = data.results[i].position[1];
+                    let pol_position = {lat:latitude,lng:longitude};
+                    var policon= new H.map.Icon('img/polmark.png',{size: {w:32,h:32}});
+                    var pol_marker = new H.map.Marker(pol_position,{icon: policon});
+                    map.addObject(pol_marker);
+                }
+            }}).catch(err => console.log(err));
+        
+        var fire_url = new URL(`https://places.ls.hereapi.com/places/v1/autosuggest?at=${browserPosition.lat},${browserPosition.lng}&q=fire&apiKey=${window.hereCreds.JS_KEY}`);
+        fetch(fire_url).then(response => response.json()).then(data => {
+            for(var i=0;i<data.results.length;i++)
+            {
+                if(data.results[i].position!=undefined)
+                {
+                    var latitude = data.results[i].position[0];
+                    var longitude = data.results[i].position[1];
+                    let fire_position = {lat:latitude,lng:longitude};
+                    var fireicon= new H.map.Icon('img/firmark.png',{size: {w:32,h:32}});
+                    var fire_marker = new H.map.Marker(fire_position,{icon: fireicon});
+                    map.addObject(fire_marker);
+                }
+            }}).catch(err => console.log(err));
+        var posIcon = new H.map.Icon('img/genmark.png',{size: {w:45,h:45}});
+        var marker = new H.map.Marker(browserPosition,{icon: posIcon});
         map.addObject(marker);
-        // var router = platform.getRoutingService(),
-        // routeRequestParams = {
-        //     mode: 'fastest;car',
-        //     representation: 'display',
-        //     routeattributes: 'waypoints,summary,shape,legs',
-        //     maneuverattributes: 'direction,action',
-        //     waypoint0: '28.4506,77.5842', // Brandenburg Gate
-        //     waypoint1: '29.1245,78.3862'  // Friedrichstraße Railway Station
-        // };
-
-
-        // router.calculateRoute(
-        // routeRequestParams,
-        // onSuccess,
-        // onError
-        // );
     });
 } else{
     alert("Geolocation not supported");
